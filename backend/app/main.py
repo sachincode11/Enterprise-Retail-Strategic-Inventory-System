fda887bb01a062562e0d590fdf4f3d7a52fca182
sachincode11 <sachincode11@gmail.com>
2026-03-27 16:37:30 +0545

    feat: setup router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import get_settings
+from app.api.routes import api_router
 
 settings = get_settings()
 
@@ -19,6 +20,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
